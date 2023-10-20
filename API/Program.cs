6cbd92dc0bf9d20beaea4bf934653e486dde3c00
fanishgupta <fanishsc11@gmail.com>
2023-10-20 11:59:51 +0530

    EndOfSection3

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
